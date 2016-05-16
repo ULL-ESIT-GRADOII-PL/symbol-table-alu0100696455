f8ebd41086d2f35178018476b669c01523f115e8
Jonathan Expósito <alu0100696455@ull.edu.es>
2016-05-16 10:50:57 +0000

    ya se crea la symbolTable

--- mainfromfile.js
+++ mainfromfile.js
@@ -1,4 +1,4 @@
-#!/usr/local/bin/node --harmony_destructuring
+#!/home/ubuntu/.nvm/versions/node/v4.4.3/bin/node --harmony_destructuring
 "use strict";
 const process = require('process');
 /* See gist: https://gist.github.com/branneman/8048520 
@@ -8,7 +8,8 @@ require('module').Module._initPaths();
 var util = require('util');
 var fs = require('fs');
 var PEG = require("pl0node.js");
-var fileName = process.argv[2] || 'tests/input5.pl0';
+var semantic = require("semantic.js");
+var fileName = process.argv[2] || 'tests/twicedecl2.pl0';
 
 const lineNumbers = (input) => {
   let count = 1;
@@ -25,6 +26,7 @@ fs.readFile(fileName, 'utf8', function (err,input) {
   console.log(`Processing <***\n${lineNumbers(input)}\n***>`);
   try {
     var r = PEG.parse(input);
+    semantic(r);
     console.log(util.inspect(r, {depth: null}));
   } catch (e) {
     //console.log(`Error en línea ${e.location.start.line} columna ${e.location.start.column}`);
